a3468dec300c1d1f220b83c884a67574a37eecd4
mdshibgatullah <mdshibgatullah94@gmail.com>
2026-09-06 22:34:12 +0600

    shib

---
+++ javascript.js
@@ -0,0 +1,96 @@
+// 1-6 random number print
+
+// method 1
+// const getRandomNum = Math.floor(Math.random() * 6) + 1;
+// console.log(getRandomNum)
+
+// method 2
+// function getRandomNum(){
+//     return Math.floor(Math.random() * 6) + 1
+// }
+// console.log(getRandomNum())
+
+
+
+// sokol name alpabetically sajanu
+// const studsents = ["Jashim", 'Rahim', 'Karim', 'Ahona', 'Bilay'];
+// console.log(studsents.sort())
+
+// sokol number serially sajanu
+
+// const numbers  = [3,5,7,1,8,0,3,56,4,86];
+// numbers.sort((a, b) => a-b) // discendng korte b-a
+// console.log(numbers)
+
+
+// leap year check
+// function leap_year(year){
+//     if(year % 4 === 0 || ((year % 400 === 0 && year % 100 !== 0))){
+//         console.log(`${year} is leap year`)
+//     }else{
+//         console.log(`${year} is not leap year`)
+//     }
+// }
+
+// leap_year(2028)
+
+// ৪ দ্বারা বিভাজ্য হলে Leap Year,
+// কিন্তু ১০০ দ্বারা বিভাজ্য হলে Leap Year নয়,
+// তবে ৪০০ দ্বারা বিভাজ্য হলে আবার Leap Year।
+
+
+// even or odd number
+
+// function number(num){
+//     if(num % 2 == 0) {
+//         console.log('Even')
+//     }else{
+//         console.log('odd')
+//     }
+// }
+// number(10)
+
+
+// power of number
+// let num = 3
+// console.log(num ** 2)
+
+// let str = "JavaScript"
+// console.log(str.length)
+
+
+
+// Reverse String 
+
+// let text = "JavaScript";
+// console.log(text.split('').reverse().join(''))
+
+// function rev(str){
+//     return str.split('').reverse().join('')
+// }
+// console.log(rev('Hello'))
+
+
+// Find biggest number
+
+// let numbers = [12,3,4,56,7,4,79,57];
+// console.log(Math.max(...numbers))
+
+// function maxN(...numbers){
+//     return Math.max(...numbers)
+// }
+// console.log(maxN(12,54,85))
+
+
+
+// array er all numbers jug kora 
+
+// let numbers = [23,4,56,74,789,23,]
+// let sum = 0;
+// for(i=0; i < numbers.length; i++){
+//     sum += numbers[i]
+// }
+// console.log(sum)
+
+
+// remove duplicate Number 
